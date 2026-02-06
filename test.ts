a0887206ee547146a3dd62c676ff4cd1ce34df11
Your Name <you@example.com>
2026-02-06 11:28:48 +0100

    add function add

--- test.ts
+++ test.ts
@@ -1,6 +1,12 @@
 function add(d: number, c: number) {
-  return c + d;
+  return c * d;
 }
 function sum(d: number, c: number) {
-  return c + d;
+  return c * d;
+}
+function alpha() {
+  return 'a';
+}
+function beta() {
+  return 'b';
 }
